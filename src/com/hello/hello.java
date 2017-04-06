a37425a70b377427dd0e9a7dde53a79dab917655
happyEveryNight <407901947@qq.com>
2017-04-07 06:53:48 +0800

    print hello world

---
+++ src/com/hello/hello.java
@@ -0,0 +1,14 @@
+package com.hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		for (int i = 0; i < 5; ++i)
+			System.out.println(i);
+		System.out.println("haha");
+	}
+
+}
